ee4582bbdc455acf8c67baef239ce1920e4aa87c
Zan <69921455+InZan123@users.noreply.github.com>
2023-03-29 12:58:45 +0200

    revamp /cotdrole

--- src/commands-manager/commands/cotd-roles.lua
+++ src/commands-manager/commands/cotd-roles.lua
@@ -5,36 +5,61 @@ local json = require"json"
 local command = {}
 
 function command.run(client, ia, cmd, args)
-    if args.make then
-        local role = args.make.role
+    if args.create then
+        local name = args.create.name or "<cotd>"
         local cotdRolesData = _G.storageManager:getData("cotdRoles", {})
         local cotdRolesRead = cotdRolesData:read()
 
-        if ia.guild.id == role.id then
-            return ia:reply("Please provide an actual role.", true)
-        end
 
-        local reminder = ""
+        
+
         if cotdRolesRead[ia.guild.id] then
-            reminder = " *(Remember, you can only have one COTD role per guild)*"
+            return ia:reply("You already have a COTD role! <@&"..cotdRolesRead[ia.guild.id].id..">", true)
         end
 
-        cotdRolesRead[ia.guild.id] = role.id
+        if not ia.guild.me:hasPermission(268435456) then
+            return ia:reply("I am unable to update this role because I don't have the \"Manage Roles\" permission.", true)
+        end
+
+        local role, err = ia.guild:createRole(name)
+
+        if not role then
+            return ia:reply("Sorry, it seems like I wasn't able to create the role. \n\nHere's the error:\n"..err, true)
+        end
+
+        local success, updateErr = _G.cotd.updateRole(role, name)
+
+        if not success then
+            return ia:reply("Sorry, it seems like I wasn't able to create the role properly. \n\nHere's the error:\n"..updateErr, true)
+        end
+
+        cotdRolesRead[ia.guild.id] = {
+            id = role.id,
+            name = name
+        }
         cotdRolesData:write(cotdRolesRead)
 
-        _G.cotd.updateRole(role)
-
-        ia:reply("Successfully made <@&"..role.id.."> a COTD role."..reminder, true)
-    else
+        ia:reply("Successfully made <@&"..role.id.."> a COTD role.\nPlease remember to not put this role above my highest role or else I wont be able to edit it.", true)
+    elseif args.remove then
         local cotdRolesData = _G.storageManager:getData("cotdRoles", {})
         local cotdRolesRead = cotdRolesData:read()
-        local roleId = cotdRolesRead[ia.guild.id]
-        if roleId == nil then
+        local roleInfo = cotdRolesRead[ia.guild.id]
+        if roleInfo == nil then
             return ia:reply("This guild does not have a COTD role.", true)
         end
         cotdRolesRead[ia.guild.id] = nil
         cotdRolesData:write(cotdRolesRead)
-        ia:reply("Successfully removed <@&"..roleId.."> as a COTD role.", true)
+        if args.remove.delete then
+            local role = ia.guild:getRole(roleInfo.id)
+            if role then
+                if not role:delete() then
+                    ia:reply("<@&"..roleInfo.id.."> is no longer a COTD role but I was unable to delete it.", true)
+                end
+            end
+            ia:reply("<@&"..roleInfo.id.."> has been successfully deleted.", true)
+        else
+            ia:reply("<@&"..roleInfo.id.."> is no longer a COTD role.", true)
+        end
     end
 end
 
@@ -46,22 +71,28 @@ command.info = {
     default_member_permissions = "0",
     options = {
         {
-            name = "make",
-            description = "Make a certain role change color based on the COTD. (The name will be overwritten)",
+            name = "create",
+            description = "Create a role which will change color based on the COTD.",
             type = dia.enums.appCommandOptionType.subCommand,
             options = {
                 {
-                    name = "role",
-                    description = "Role to change color.",
-                    type = dia.enums.appCommandOptionType.role,
-                    required = true
+                    name = "name",
+                    description = "The name of the role. <cotd> is replaced by the name of the color. (Default: <cotd>)",
+                    type = dia.enums.appCommandOptionType.string
                 }
             }
         },
         {
             name = "remove",
             description = "Stop changing the color of your COTD role.",
-            type = dia.enums.appCommandOptionType.subCommand
+            type = dia.enums.appCommandOptionType.subCommand,
+            options = {
+                {
+                    name = "delete",
+                    description = "If you wanna delete the role from the guild or not. (Default: False)",
+                    type = dia.enums.appCommandOptionType.boolean
+                }
+            }
         }
     }
 }
--- src/cotd.lua
+++ src/cotd.lua
@@ -35,15 +35,30 @@ function cotd.getCurrentDay()
     return math.floor(os.time()/86400)
 end
 
-function cotd.updateRole(role)
+function cotd.updateRole(role, name)
     local currentColor = cotd.getCurrentColor()
+    if currentColor == nil then
+        return false, "currentColor is nil idk why"
+    end
     local newColor = dia.Color.fromHex(currentColor.color)
-    role:setColor(newColor)
-    role:setName(currentColor.name)
-    
+    local colorSuccess, colorErr = role:setColor(newColor)
+    local renameSuccess, renameErr = role:setName(name:gsub("<cotd>", currentColor.name))
+    local err = (colorErr and (colorErr.."\n") or "")..(renameErr or "")
+    return (colorSuccess and renameSuccess) and true or false, err
 end
 
 function cotd:startLoop(client)
+    client:on("roleDelete", function (role)
+        local cotdRolesData = _G.storageManager:getData("cotdRoles", {})
+        local cotdRolesRead = cotdRolesData:read()
+        local guildCotdRole = cotdRolesRead[role.guild.id]
+        if guildCotdRole then
+            if guildCotdRole.id == role.id then
+                cotdRolesRead[role.guild.id] = nil
+                cotdRolesData:write(cotdRolesRead)
+            end
+        end
+    end)
     coroutine.wrap(function()
         while true do
             timer.sleep(1000)
@@ -62,10 +77,34 @@ function cotd:startLoop(client)
             local cotdRolesData = _G.storageManager:getData("cotdRoles", {})
             local cotdRolesRead = cotdRolesData:read()
 
-            for guildId, roleId in pairs(cotdRolesRead) do
+            local update = false
+            local removeGuilds = {}
+
+            for guildId, roleInfo in pairs(cotdRolesRead) do
+                if type(roleInfo) ~= "table" then
+                    roleInfo = {
+                        id = roleInfo,
+                        name = "<cotd>"
+                    }
+                    cotdRolesRead[guildId] = roleInfo
+                    update = true
+                end
                 local guild = client:getGuild(guildId)
-                local role = guild:getRole(roleId)
-                cotd.updateRole(role)
+                local role = guild:getRole(roleInfo.id)
+                if not role then
+                    table.insert(removeGuilds,guildId)
+                else
+                    cotd.updateRole(role, roleInfo.name)
+                end
+            end
+
+            for i, v in pairs(removeGuilds) do
+                cotdRolesRead[v] = nil
+                update = true
+            end
+
+            if update then
+                cotdRolesData:write(cotdRolesRead)
             end
             ::continue::
         end
